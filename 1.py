2259aaabde3f2e25d66505854b68cf45f890a617
mellowdrifter <mellow.drifter@gmail.com>
2014-03-30 20:38:10 +0100

    Updated readme Added tuple

--- 1.py
+++ 1.py
@@ -2,22 +2,21 @@
 import telnetlib
 import getpass
 
-#get hostname, username, and password
-host=input("Please enter a host name: ")
+#use a tuple to set list of devices to log into
+devices=("vr1.wok1.hso.uk.com","cr1.263992.hso.uk.com")
+
+#get username and password
 user=input("Enter your username: ")
 password=getpass.getpass(prompt="Enter your password: ")
 
 #Log into host
-tn = telnetlib.Telnet(host)
-tn.read_until(b"Username: ")
-tn.write(user.encode('ascii') + b"\n")
-tn.read_until(b"Password: ")
-tn.write(password.encode('ascii') + b"\n")
+for i in devices:
+	tn = telnetlib.Telnet(i)
+	tn.read_until(b"Username: ")
+	tn.write(user.encode('ascii') + b"\n")
+	tn.read_until(b"Password: ")
+	tn.write(password.encode('ascii') + b"\n")
+	tn.write(b"show ip ospf int brief\n")
+	tn.write(b"exit\n")
+	print(tn.read_all().decode('ascii'))
 
-#get required information, then exit from host
-tn.write(b"show ip ospf int brief\n")
-tn.write(b"show run | sec router ospf\n")
-tn.write(b"exit\n")
-
-#display collected information
-print(tn.read_all().decode('ascii'))
