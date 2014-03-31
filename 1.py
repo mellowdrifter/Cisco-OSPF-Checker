33fb50aaa35281e2b06dc2f1ae316b46a379b4d0
mellowdrifter <mellow.drifter@gmail.com>
2014-03-31 14:25:32 +0100

    Now includes a check for an enable password and enters is when needed

--- 1.py
+++ 1.py
@@ -1,22 +1,33 @@
-#import needed modules
+#!/usr/bin/python3
+
+'''Simple script to log into a number of devices
+and check certain OSPF properties'''
+
 import telnetlib
 import getpass
 
-#use a tuple to set list of devices to log into
-devices=("vr1.wok1.hso.uk.com","cr1.263992.hso.uk.com")
+devices=("cr1.263992.hso.uk.com","cr1.303953.hso.uk.com")
 
 #get username and password
 user=input("Enter your username: ")
 password=getpass.getpass(prompt="Enter your password: ")
+enablepass=getpass.getpass(prompt="Enter your enable password: ")
 
 #Log into host
 for i in devices:
-	tn = telnetlib.Telnet(i)
-	tn.read_until(b"Username: ")
-	tn.write(user.encode('ascii') + b"\n")
-	tn.read_until(b"Password: ")
-	tn.write(password.encode('ascii') + b"\n")
-	tn.write(b"show ip ospf int brief\n")
-	tn.write(b"exit\n")
-	print(tn.read_all().decode('ascii'))
+    tn = telnetlib.Telnet(i,23,5)
+    tn.read_until(b"Username: ")
+    tn.write(user.encode('ascii') + b"\n")
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+    tn.write(b"\n")
+    en=(tn.read_some().decode('ascii'))
+    if ">" in en:
+        tn.write(b"enable\n")
+        tn.write(enablepass.encode('ascii') + b"\n")
+    tn.write(b"terminal length 0\n")  
+    tn.write(b"show ip ospf int brief\n")
+    tn.write(b"sh ip ospf neighbor\n")
+    tn.write(b"exit\n")
+    print(tn.read_all().decode('ascii'))
 
