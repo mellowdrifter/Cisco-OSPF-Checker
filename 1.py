be93aec109e0935b9c7fe0cc761e78ec3ef9937a
mellowdrifter <mellow.drifter@gmail.com>
2014-04-01 10:06:38 +0100

    App will now read list of devices from a text file. App will check if the file exists and exit if not available.

--- 1.py
+++ 1.py
@@ -8,12 +8,23 @@ import getpass
 import sys
 import os
 
-devices=("cr1.263992.hso.uk.com","cr1.303953.hso.uk.com")
+devices=[]
+try:
+    with open('devices.txt') as file:
+        pass
+except IOError as e:
+    print("\ndevices.txt does not exist or unreadable, exiting now")
+    sys.exit()
+
+f = open('devices.txt')
+for line in f:
+    devices.append(line.rstrip())
+    
 try:
     with open('report.txt') as file:
-        choice=input("\nreport.txt already exists, do you want to overwrite it? [Yes/No]: ")
-        lchoice=choice.lower()
-        if lchoice[0]=="n":
+        choice = input("\nreport.txt already exists, do you want to overwrite it? [Yes/No]: ")
+        lchoice = choice.lower()
+        if lchoice[0] == "n":
             sys.exit()
         else:
             print("\nreport.txt will be overwritten!")
@@ -24,9 +35,9 @@ except IOError as e:
 
 
 #get username and password
-user=input("\n\nEnter your username: ")
-password=getpass.getpass(prompt="Enter your password: ")
-enablepass=getpass.getpass(prompt="Enter your enable password: ")
+user = input("\n\nEnter your username: ")
+password = getpass.getpass(prompt="Enter your password: ")
+enablepass = getpass.getpass(prompt="Enter your enable password: ")
 
 #Log into host
 for i in devices:
@@ -36,7 +47,7 @@ for i in devices:
     tn.read_until(b"Password: ")
     tn.write(password.encode('ascii') + b"\n")
     tn.write(b"\n")
-    en=(tn.read_some().decode('ascii'))
+    en = (tn.read_some().decode('ascii'))
     if ">" in en:
         tn.write(b"enable\n")
         tn.write(enablepass.encode('ascii') + b"\n")
@@ -47,6 +58,6 @@ for i in devices:
     tn.write(b"sh ip ospf neighbor\n")
     tn.write(b"exit\n")
     output=(tn.read_all().decode('ascii'))
-    f=open('report.txt', 'a')
+    f = open('report.txt', 'a')
     f.write(output)
     f.close
