c811d8cffcda7bab3a59adc5eefeba5c6f234ea0
mellowdrifter <mellow.drifter@gmail.com>
2014-04-11 18:53:16 +0100

    Output is now a series of lists

--- 1.py
+++ 1.py
@@ -19,8 +19,8 @@ except IOError as e:
 f = open('devices.txt')
 for line in f:
     devices.append(line.rstrip())
-    
-#Check if report.txt exists. If so, ask if we want to overwrite it    
+
+#Check if report.txt exists. If so, ask if we want to overwrite it
 try:
     with open('report.txt') as file:
         choice = input("\nreport.txt already exists. Do you want to overwrite it? [y/n]: ")
@@ -55,24 +55,40 @@ for device in devices:
         tn.write(b"enable\n")
         tn.write(enablepass.encode('ascii') + b"\n")
     print("Checking ",device)
+    f = open('report.txt', 'a')
+    f.write("\nChecking "+device)
     tn.write(b"terminal length 0\n")
     tn.write(b"show ver | include IOS\n")
     tn.write(b"show inventory\n")
     tn.write(b"show ip ospf interface\n")
     tn.write(b"exit\n")
     output=(tn.read_all().decode('ascii'))
-    i = re.findall('(?:GigabitEthernet|FastEthernet|Serial|Tunnel|Loopback|Dialer|BVI)[0-9]{1,4}/?[0-9]{0,4}.?[0-9]{0,4}/?[0-9]{0,3}/?[0-9]{0,3}/?[0-9]{0,3}:?[0-9]{0,3}',output)
-    ip = re.findall(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})',output)
-    A = re.findall(r'Area[\s]{0,3}[0-9]{1,5}',output)
-    n = re.findall(r'Network Type[\s]{0,3}[a-zA-Z_]{0,20}',output)
-    C = re.findall(r'Cost:[\s]{0,3}[0-9]{1,5}',output)
-    h = re.findall(r'Hello[\s][0-9]{1,3}',output)
-    D = re.findall(r'Dead[\s][0-9]{1,3}',output)
-    for a,b,c,d,e,f,g in zip(i,ip,A,n,C,h,D):
-        print("\n"+a)
-        print(b)
-        print(c)
-        print(d)
-        print(e)
-        print(f)
-        print(g)
+    new = re.split(r'[\n](?=GigabitEthernet|FastEthernet|Serial|Tunnel|Loopback|Dialer|BVI)',output)
+    for i in new:
+        interface = re.findall(r'(?:GigabitEthernet|FastEthernet|Serial|Tunnel|Loopback|Dialer|BVI)[0-9]{1,4}/?[0-9]{0,4}.?[0-9]{0,4}/?[0-9]{0,3}/?[0-9]{0,3}/?[0-9]{0,3}:?[0-9]{0,3}',i)
+        ip = re.findall(r'Internet Address (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})',i)
+        area = re.findall(r'Area ([\s]{0,3}[0-9]{1,5})',i)
+        net = re.findall(r'Network Type ([\s]{0,3}[a-zA-Z_]{0,20})',i)
+        cost = re.findall(r'Cost: ([0-9]{1,5})',i)
+        hello = re.findall(r', Hello ([0-9]{1,3})',i)
+        dead = re.findall(r', Dead ([0-9]{1,3})',i)
+        if not interface:
+            continue
+        print("\nInterface",interface)
+        f.write("\n\nInterface: "+str(interface))
+        print("IP:",ip)
+        f.write("\nIP: "+str(ip))
+        print("Area:",area)
+        f.write("\nArea: "+str(area))
+        print("Type:",net)
+        f.write("\nType: "+str(net))
+        print("Cost",cost)
+        f.write("\nCost: "+str(cost))
+        if hello:
+            print("Hello:",hello)
+            f.write("\nHello: "+str(hello))
+        if dead:
+            print("Dead:",dead)
+            f.write("\nDead: "+str(dead))
+    f.close()
+            
