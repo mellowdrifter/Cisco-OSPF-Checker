9b15780b94115ed3a801ad5017466f4270ba6e8e
mellowdrifter <mellow.drifter@gmail.com>
2014-05-01 17:12:01 +0100

    Moved definitions into a single definition

--- ospf.py
+++ ospf.py
@@ -10,37 +10,32 @@ import sys
 
 
 def interface(i):
-    ospf_int = re.findall(r'(?:GigabitEthernet|FastEthernet|Serial|Tunnel|Loopback|Dialer|BVI|Vlan|Virtual-Access)[0-9]{1,4}/?[0-9]{0,4}.?[0-9]{0,4}/?[0-9]{0,3}/?[0-9]{0,3}/?[0-9]{0,3}:?[0-9]{0,3}',i)
-    if ospf_int:
-        return ospf_int
+    ospf_int = re.search(r'(GigabitEthernet|FastEthernet|Serial|Tunnel|Loopback|Dialer|BVI|Vlan|Virtual-Access)[0-9]{1,4}/?[0-9]{0,4}.?[0-9]{0,4}/?[0-9]{0,3}/?[0-9]{0,3}/?[0-9]{0,3}:?[0-9]{0,3}',i)
     if not ospf_int:
         return None
-
-def getip(i):
-    ip = re.findall(r'Internet Address (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})',i)
+    ospf_int = ospf_int.group()
+    ip = re.search(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2})',i)
+    ip = ip.group()
     if not ip:
-        ip = re.findall(r'Interface is unnumbered. Using address of [a-zA-Z]{1,10}[0-9]{1,5}/?[0-9]{0,5}.?[0-9]{0,5}',i)
-    return ip
-
-def getarea(i):
-    area = re.findall(r'Area ([\s]{0,3}[0-9]{1,5})',i)
-    return area
-
-def gettype(i):
-    net = re.findall(r'Network Type ([\s]{0,3}[a-zA-Z_]{0,20})',i)
-    return net
-
-def getcost(i):
-    cost = re.findall(r'Cost: ([0-9]{1,5})',i)
-    return cost
-
-def gethello(i):
-    hello = re.findall(r', Hello ([0-9]{1,3})',i)
-    return hello
-
-def getdead(i):
-    dead = re.findall(r', Dead ([0-9]{1,3})',i)
-    return dead
+        ip = re.search(r'Interface is unnumbered. Using address of [a-zA-Z]{1,10}[0-9]{1,5}/?[0-9]{0,5}.?[0-9]{0,5}',i)
+        ip = ip.group()
+    a = re.search(r'Area ([\s]{0,3}[0-9]{1,5})',i)
+    area = a.group(1)
+    n = re.search(r'Network Type ([\s]{0,3}[a-zA-Z_]{0,20})',i)
+    net = n.group(1)
+    c = re.search(r'Cost: ([0-9]{1,5})',i)
+    cost = c.group(1)
+    h = re.search(r'Hello ([0-9]{1,3})',i)
+    if not h:
+        hello = "N/A"
+    else:
+        hello = h.group(1)
+    d = re.search(r'Dead ([0-9]{1,3})',i)
+    if not d:
+        dead = "N/A"
+    else:
+        dead = d.group(1)
+    return (ospf_int,ip,area,net,cost,hello,dead)
 
 
 #Get list of devices to check
@@ -92,7 +87,6 @@ except IOError as e:
 #Get username and password
 user = input("\n\nEnter your username: ")
 password = getpass.getpass(prompt="Enter your password: ")
-enablepass = getpass.getpass(prompt="Enter your enable password: ")
 
 #Log into host, run commands, echo output, write output to file
 for device in devices:
@@ -103,10 +97,6 @@ for device in devices:
         tn.read_until(b"Password: ")
         tn.write(password.encode('ascii') + b"\n")
         tn.write(b"\n")
-        en = (tn.read_some().decode('ascii'))
-        if ">" in en:
-            tn.write(b"enable\n")
-            tn.write(enablepass.encode('ascii') + b"\n")
         print("\n\nChecking ",device)
         f = open('report.txt', 'a')
         f.write("\n\n\nChecking "+device)
@@ -118,27 +108,19 @@ for device in devices:
         if raw:
             raw_out+=output
         ospf = re.split(r'[\n](?=GigabitEthernet|FastEthernet|Serial|Tunnel|Loopback|Dialer|BVI|Vlan|Virtual-Access)',output)
-        for i in ospf:
-            intf = interface(i)
-            if not intf:
+        for o in ospf:
+            data = interface(o)
+            if not data:
                 continue
-            ip = getip(i)
-            area = getarea(i)
-            net = gettype(i)
-            cost = getcost(i)
-            hello = gethello(i)
-            dead = getdead(i)
-            print("\nInt:\t{}\nIP:\t{}\nArea:\t{}\nNet:\t{}\nCost:\t{}".format(intf[0],ip[0],area[0],net[0],cost[0]))
-            f.write("\n\nInt:\t"+intf[0]+"\nIP:\t"+ip[0]+"\nArea:\t"+area[0]+"\nType:\t"+net[0]+"\nCost:\t"+cost[0])
-            if hello:
-                print("Hello:\t"+hello[0])
-                f.write("\nHello:\t"+hello[0])
-            if dead:
-                print("Dead:\t"+dead[0])
-                f.write("\nDead:\t"+dead[0])
+            print("\nInt:\t{}\nIP:\t{}\nArea:\t{}\nNet:\t{}\nCost:\t{}".format(data[0],data[1],data[2],data[3],data[4]))
+            if data[5]:
+                print("Hello:\t{}".format(data[5]))
+            if data[6]:
+                print("Dead:\t{}".format(data[6]))
     except:
         print("\n!*Unable to resolve or log into",device,"*!")
-    f.close()            
+
+f.close()            
 
 if raw:
     print("\n\nWriting raw output to raw.txt")
